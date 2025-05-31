9ecaa0352ad8e3b3fc1dc220eaafe270a84d64be
Shivam-hub976 <kumarshivambgs407@gmail.com>
2025-05-31 13:57:03 +0530

    routes added

--- app.py
+++ app.py
@@ -2,9 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Routes
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/docs')
+def docs():
+    return render_template('docs.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
