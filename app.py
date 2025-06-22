ce8c928c169566efb93ba523f64dcf60fdca076d
Shivam-hub976 <kumarshivambgs407@gmail.com>
2025-06-22 19:34:55 +0530

    some error tracking

--- app.py
+++ app.py
@@ -87,6 +87,12 @@ class Feedback(db.Model):
     recommend = db.Column(db.String(10))
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
+
+
+with app.app_context():
+    db.create_all()
+
+
 def parse_stress(stress_value):
     if isinstance(stress_value, (int, float)):
         return float(stress_value)
